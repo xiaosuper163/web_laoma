1681e090a8651b2e1597039113b17e9dc4911099
xiaosuper163 <yabing@sfu.ca>
2019-01-04 23:21:03 -0800

    update

--- node/01test.js
+++ node/01test.js
@@ -1,3 +1,15 @@
 console.log("Hi node js!");
 var a = 9;
 console.log(++a);
+console.error("123456");
+console.log(Object.prototype);
+console.time("123");
+var sum = 0;
+for (var i=0; i<10000; i++) {
+    sum += i;
+}
+console.timeEnd("123");
+
+setInterval(function() {
+    console.log(Date())
+}, 500);
